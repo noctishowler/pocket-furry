89ecee55e9519e3878b100ce963048339612f991
noctishowler <noctishowler@gmail.com>
2026-09-13 22:05:33 -0700

    Create character.js

---
+++ assets/characters/noctis/character.js
@@ -0,0 +1,107 @@
+"use strict";
+
+window.PocketFurryCharacters =
+  window.PocketFurryCharacters || {};
+
+window.PocketFurryCharacters.noctis = {
+  id: "noctis",
+  name: "Noctis",
+  path: "assets/characters/noctis/",
+
+  animations: {
+    idle: {
+      right: "idle.gif",
+      left: "idle-left.gif"
+    },
+
+    idleFront: {
+      right: "idle-front.gif",
+      left: "idle-front.gif"
+    },
+
+    chaseBall: {
+      right: "chase-ball.gif",
+      left: "chase-ball.gif"
+    },
+
+    clean: {
+      right: "clean.gif",
+      left: "clean.gif"
+    },
+
+    happy: {
+      right: "happy.gif",
+      left: "happy-left.gif"
+    },
+
+    sad: {
+      right: "sad.gif",
+      left: "sad-left.gif"
+    },
+
+    angry: {
+      right: "angry.gif",
+      left: "angry-left.gif"
+    },
+
+    bored: {
+      right: "bored.gif",
+      left: "bored-left.gif"
+    },
+
+    hungry: {
+      right: "hungry.gif",
+      left: "hungry-left.gif"
+    },
+
+    sick: {
+      right: "sick.gif",
+      left: "sick-left.gif"
+    },
+
+    sleepy: {
+      right: "sleepy.gif",
+      left: "sleepy-left.gif"
+    },
+
+    lieDown: {
+      right: "lie-down.gif",
+      left: "lie-down.gif"
+    },
+
+    sleep: {
+      right: "sleep.gif",
+      left: "sleep-left.gif"
+    },
+
+    wakeUp: {
+      right: "wake-up.gif",
+      left: "wake-up-left.gif"
+    },
+
+    eat: {
+      right: "eat.gif",
+      left: "eat-left.gif"
+    },
+
+    walk: {
+      right: "walk-right.gif",
+      left: "walk-left.gif"
+    },
+
+    jump: {
+      right: "jump-right.gif",
+      left: "jump-left.gif"
+    },
+
+    bound: {
+      right: "bound-right.gif",
+      left: "bound-left.gif"
+    },
+
+    death: {
+      right: "death.gif",
+      left: "death-left.gif"
+    }
+  }
+};
