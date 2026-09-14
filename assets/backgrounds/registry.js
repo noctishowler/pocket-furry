7992ebff4b44639d47ca792bb08216b86444b917
noctishowler <noctishowler@gmail.com>
2026-09-13 22:33:22 -0700

    Create registry.js

---
+++ assets/backgrounds/registry.js
@@ -0,0 +1,46 @@
+"use strict";
+
+
+window.PocketFurryBackgrounds = {
+
+  home: {
+    id: "home",
+    name: "Home",
+    file: "background-home.PNG"
+  },
+
+  outside: {
+    id: "outside",
+    name: "Outside",
+    file: "background-outside.PNG"
+  },
+
+  daywoods: {
+    id: "daywoods",
+    name: "Day Woods",
+    file: "background-daywoods.PNG"
+  },
+
+  nightwoods: {
+    id: "nightwoods",
+    name: "Night Woods",
+    file: "background-nightwoods.PNG"
+  },
+
+  nightcity: {
+    id: "nightcity",
+    name: "Night City",
+    file: "background-nightcity.PNG"
+  },
+
+  camping: {
+    id: "camping",
+    name: "Camping",
+    file: "background-camping.PNG"
+  }
+
+};
+
+
+window.PocketFurryDefaultBackground =
+  "home";
